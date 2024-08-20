cf6b90c6f3b05fdfbb8843a9cebfcde2695ef5f3
Quang Phan <phanquang1812@gmail.com>
2024-08-20 11:19:33 +0700

    react-tiktok

---
+++ src/Component/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function App(){
+  return (
+    <div className='App'>
+      <div className='header'>
+        <div>
+          <p>Logo</p>
+        </div>
+      </div>
+      <div className='main-container'>
+        <div className='sidebar'>sidebar</div>
+        <div className='content'>content</div>
+      </div>
+    </div>
+  )
+}
